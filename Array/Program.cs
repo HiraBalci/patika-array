34d43b57bd31be7306672c8931f6833cff06cc28
HiraBalci <93313737+HiraBalci@users.noreply.github.com>
2022-01-31 11:29:40 +0300

    array

---
+++ Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi tanımlama
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "kedi", "köpek", "kuş", "maymun" };
+            int[] dizi;
+            dizi = new int[3];
+            //Dizilere Değer Atama ve Erişim
+            renkler[0] = "Mavi";
+            Console.WriteLine(hayvanlar[0]);
+            dizi[2] = 10;
+            Console.WriteLine(dizi[2]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngülerle dizi kullanımı
+            //klavyeden girilen n tane sayının ort. hesaplayan program
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz:");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi=new int[diziUzunlugu];
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayısı giriniz : ", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var item in sayiDizisi)
+            {
+                toplam += item;
+            }
+                Console.WriteLine("ortalama:"+toplam/diziUzunlugu);
+            
+        }
+    }
+}
